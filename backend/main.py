417fc9109c3e75d5c99cbd9fb709d1db9a186454
Rohit2133 <144320192+Rohit2133@users.noreply.github.com>
2025-12-09 18:47:46 +0530

    Change server port to 8000
    Set the server port to 8000 instead of using environment variable.

--- backend/main.py
+++ backend/main.py
@@ -90,5 +90,4 @@ def root():
 
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=os.getenv("RENDER", "") == "")
+   uvicorn.run("main:app", host="0.0.0.0", port=8000)
